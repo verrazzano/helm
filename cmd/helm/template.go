aa692c0917601da9d6fb32a4413c40ff27902fde
Daishan Peng <daishan@rancher.com>
2018-04-19 11:55:31 -0700

    only show notes when --notes

--- cmd/helm/template.go
+++ cmd/helm/template.go
@@ -261,7 +261,11 @@ func (t *templateCmd) run(cmd *cobra.Command, args []string) error {
 		}
 		data := m.Content
 		b := filepath.Base(m.Name)
-		if !t.showNotes && b == "NOTES.txt" {
+		if t.showNotes {
+			if b != "NOTES.txt" {
+				continue
+			}
+		} else if b == "NOTES.txt" {
 			continue
 		}
 		if strings.HasPrefix(b, "_") {
